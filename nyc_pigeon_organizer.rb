03e6ab3b7ca5568a16f1a38fff1102dd41102914
Hailey Ringier <haileyringier@outlook.com>
2020-06-15 17:45:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  
+  data.each do |key1, value|
+    value.each do |key2, array| 
+      array.each do |element|
+        pigeon_list[element] = {}
+       #pigeon_list[element].merge(key1)  
+     
+     
+      end
+    end
+  end
+  puts pigeon_list
+  pigeon_list
 end
